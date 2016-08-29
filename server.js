31a02adfde5b75cd653739c418bfdc449705a9ac
John Smalley <john.m.smalley@gmail.com>
2016-08-29 17:50:07 -0500

    Server is now able to load index.html located in public folder

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.send('Hello world');
 })
 
-module.exports = app;
+var server = app.listen(process.env.PORT || 3000, function() {
+  var port = server.address().port;
+  console.log("Server listening on port: ", port);
+});
+
+
+
